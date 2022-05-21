8401be5b694a68cc9040758128625a4b4a9651f2
Dmitry <dimachizhikov2@gmail.com>
2022-05-21 14:05:13 +0300

    url, placeholder view, and first-cut templates for my_lists

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -5,6 +5,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 MAX_WAIT = 10
 
@@ -58,3 +59,12 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.find_element(by=By.NAME, value='email')
         navbar = self.browser.find_element(by=By.CSS_SELECTOR, value='.navbar')
         self.assertNotIn(email, navbar.text)
+
+    def add_list_item(self, item_text):
+        num_rows = len(
+            self.browser.find_elements(by=By.CSS_SELECTOR, value='#id_list_table tr'))
+
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
